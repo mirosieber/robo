0e0947580c9ae203c8a86ed8f632b993e5883096
mirosieber <miro.sieber@gmx.ch>
2025-12-30 20:31:37 +0100

    hardware

--- src/robo_control/src/hardware.cpp
+++ src/robo_control/src/hardware.cpp
@@ -4,6 +4,9 @@
 #include "pluginlib/class_list_macros.hpp"
 #include "rclcpp/rclcpp.hpp"
 
+#include <pigpio.h>
+#include <iostream>
+
 #include <vector>
 #include <string>
 
@@ -12,10 +15,52 @@ namespace robo_control
 
 class Hardware : public hardware_interface::SystemInterface
 {
+
+private:
+  int steerServoPin{}; // PWM0 pin
+  int trottleServoPin{}; // PWM1 pin
+  void initGPIO(){
+    if (gpioInitialise() < 0){
+      std::cerr << "Pigpio initialization failed!" << std::endl;
+    }
+  }
+  void closeGPIO(){
+    gpioTerminate();
+  }
+  void setVel(double velocity){
+    // Implement servo/motor control for drive velocity
+    // Convert velocity to pulse width in microseconds (1ms to 2ms)
+    uint16_t pulseWidth = static_cast<uint16_t>(1500 + (velocity * 500)); // Assuming velocity in range [-1, 1]
+    gpioServo(trottleServoPin, pulseWidth);
+  }
+
+  void setSteerPos(double angle){
+    // Implement servo control for steering position
+    // Convert angle to pulse width in microseconds (1ms to 2ms)
+    uint16_t pulseWidth = static_cast<uint16_t>(1000 + ((angle * 1000) / 180.0)); // Assuming angle in range [-90, 90]
+    gpioServo(steerServoPin, pulseWidth);
+  }
+
+
 public:
   hardware_interface::CallbackReturn on_init(
-    const hardware_interface::HardwareInfo & /*info*/) override
+    const hardware_interface::HardwareInfo & info) override
   {
+    try {
+      steerServoPin = std::stoi(info.hardware_parameters.at("steer_servo_pin"));
+      trottleServoPin = std::stoi(info.hardware_parameters.at("drive_motor_pin"));
+    } catch (const std::exception& e) {
+      std::cerr << "Failed to read servo pins from URDF: " << e.what() << std::endl;
+      return hardware_interface::CallbackReturn::ERROR;
+    }
+    initGPIO();
+    return hardware_interface::CallbackReturn::SUCCESS;
+  }
+
+  hardware_interface::CallbackReturn on_shutdown(
+    const rclcpp::State & /*previous_state*/) override
+  { 
+    closeGPIO();
     return hardware_interface::CallbackReturn::SUCCESS;
   }
 
@@ -53,6 +98,10 @@ public:
     // Apply commands to state variables for simulation
     drive_vel_ = drive_cmd_;
     steer_pos_ = steer_cmd_;
+
+    //Apply commands to hardware here in a real implementation
+    setVel(drive_cmd_);
+    setSteerPos(steer_cmd_);
     
     return hardware_interface::return_type::OK;
   }
